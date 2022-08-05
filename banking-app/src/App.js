a6b6a4ad412840a5d5f6744cb0638f1c865bdb57
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-05 17:29:46 +0800

    Added other Components

--- banking-app/src/App.js
+++ banking-app/src/App.js
@@ -3,11 +3,13 @@ import React from 'react'
 //Component
 import UserDisplay from './Components/UserDisplay'
 import Navbar from './Components/Navbar'
+import Clock from './Components/Clock'
 
 function App() {
   return (
     <>
       <Navbar />
+
       <UserDisplay />
     </>
   )
--- banking-app/src/Components/Navbar.jsx
+++ banking-app/src/Components/Navbar.jsx
@@ -3,6 +3,18 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
+      <nav className='navbar' role='navigation' aria-label='main navigation'>
+        <div className='navbar-brand'>
+          <h1 className='title'>BANK OF THE PHILIPPINES</h1>
+
+          <a role='button' className='navbar-burger' aria-label='menu' aria-expanded='false'>
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
+          </a>
+        </div>
+        
+      </nav>
     </>
   )
 }
