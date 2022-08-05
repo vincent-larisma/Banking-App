170134d63e9b3bead98790639914e625d389e382
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-06 06:52:21 +0800

    Update LoginPage.jsx

--- banking-app/src/Components/LoginPage.jsx
+++ banking-app/src/Components/LoginPage.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 //Component
 import Navbar from './Navbar'
 import Footer from './Footer'
 
 export default function LoginPage() {
+  const [dataBase, setDataBase] = useState({
+    name: '',
+    password: '',
+    userType: '',
+  })
+
+  const { name, password, userType } = dataBase
+
+  const dataBaseList = [{ name: 'admin', password: 'admin', userType: 'admin' }]
+
+  const handleChangeName = (e) => {
+    const { value, name } = e.target
+    console.log(dataBase)
+    setDataBase({ ...dataBase, [name]: value })
+  }
+  const handleChangePass = (e) => {
+    const { value, name } = e.target
+    console.log(dataBase)
+    setDataBase({ ...dataBase, [name]: value })
+  }
+
   return (
     <>
       <Navbar />
       <div className='container grid login-form flex-container '>
-        <div className='field m-6 grid-login notification grid-center'>
+        <div className='field m-6 grid-login notification grid-center is-info is-light'>
           <div className='title text-center'>Login</div>
           <div className='field'>
             <p className='control has-icons-left has-icons-right'>
-              <input className='input' type='text' placeholder='Username' />
+              <input
+                className='input'
+                type='text'
+                placeholder='Username'
+                name='name'
+                value={name}
+                onChange={handleChangeName}
+              />
               <span className='icon is-small is-left'>
                 <i className='fa-solid fa-user'></i>
               </span>
@@ -21,7 +49,14 @@ export default function LoginPage() {
           </div>
           <div className='field'>
             <p className='control has-icons-left'>
-              <input className='input' type='password' placeholder='Password' />
+              <input
+                className='input'
+                type='password'
+                placeholder='Password'
+                name='password'
+                value={password}
+                onChange={handleChangePass}
+              />
               <span className='icon is-small is-left'>
                 <i className='fas fa-lock'></i>
               </span>
