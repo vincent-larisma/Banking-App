7a4cd42305a4667077e8878b6bc8a21a0995dc4f
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-08 18:41:06 +0800

    Update Revert Changes

--- banking-app/src/Components/Modal.jsx
+++ banking-app/src/Components/Modal.jsx
@@ -2,65 +2,46 @@ import React from 'react'
 import { useState } from 'react'
 
 export default function Modal(props) {
+  const [deposit, setDeposit] = useState({
+    depositValue: '',
+  })
 
-const [deposit, setDeposit] = useState({
-  depositValue: ''
-})
+  const [withdraw, setWithdraw] = useState({
+    withdrawValue: '',
+  })
 
-const [withdraw, setWithdraw] = useState({
-  withdrawValue: ''
-})
+  const [transfer, setTransfer] = useState({
+    transferValue: '',
+  })
 
-const [transfer, setTransfer] = useState({
-  transferValue: ''
-})
+  const [transferId, setTransferId] = useState({
+    transferNewId: '',
+  })
 
-const [transferId, setTransferId] = useState({
-  transferNewId: ''
-})
+  const { depositValue } = deposit
+  const { withdrawValue } = withdraw
+  const { transferValue } = transfer
+  const { transferNewId } = transferId
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 01ee73e (Merge pull request #13 from genalv/gene-branch)
   const handleChangeDeposit = (event) => {
     const { value, name } = event.target
     setDeposit({ ...deposit, [name]: value })
   }
-<<<<<<< HEAD
-=======
-// const handleUserToTransfer = (e) => {
-//   const { value, name } = e.target
-//   setNewUserBalance({ ...newUserBalance, [name]: value })
-// }
 
-const { depositValue } = deposit;
-const { withdrawValue } = withdraw;
-const { transferValue } = transfer;
-const { transferNewId } = transferId;
->>>>>>> parent of 215a062 (update)
-=======
->>>>>>> parent of 01ee73e (Merge pull request #13 from genalv/gene-branch)
+  const handleChangeWithdraw = (event) => {
+    const { value, name } = event.target
+    setWithdraw({ ...withdraw, [name]: value })
+  }
 
-const handleChangeDeposit = (event) => {
-  const { value, name } = event.target
-  setDeposit({ ...deposit, [name]: value })
-}
+  const handleChangeTransfer = (event) => {
+    const { value, name } = event.target
+    setTransfer({ ...transfer, [name]: value })
+  }
+  const handleTransferId = (event) => {
+    const { value, name } = event.target
+    setTransferId({ ...transferNewId, [name]: value })
+  }
 
-const handleChangeWithdraw = (event) => {
-  const { value, name } = event.target
-  setWithdraw({ ...withdraw, [name]: value })
-}
-
-const handleChangeTransfer = (event) => {
-  const { value, name } = event.target
-  setTransfer({ ...transfer, [name]: value })
-}
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 01ee73e (Merge pull request #13 from genalv/gene-branch)
   const handleSubmit = () => {
     if (props.method === 'deposit') {
       props.handleClickNewDeposit(parseInt(depositValue))
@@ -147,53 +128,8 @@ const handleChangeTransfer = (event) => {
             </button>
             <button className='close-modal delete' onClick={props.toggleModal}></button>
           </div>
-<<<<<<< HEAD
-=======
-const handleTransferId = (event) => {
-  const { value, name } = event.target
-  setTransferId({ ...transferNewId, [name]: value})
-  console.log(transferNewId)
-=======
         </div>
       )}
     </>
   )
->>>>>>> parent of 01ee73e (Merge pull request #13 from genalv/gene-branch)
-}
-
-const handleSubmit = () =>{
-if (props.method ==='deposit'){
-  props.handleClickNewDeposit(parseInt(depositValue))
-}
-if (props.method==='withdraw'){
-  props.handleClickNewWithdraw(parseInt(withdrawValue))
-}
-if (props.method==='transfer'){
-  props.handleClickNewTransfer(parseInt(transferValue), transferNewId)
-}
-} 
-
-
-return (
-  <>
-
-    {props.isOpen && (
-      <div id='modalcomponent' className="modal active-modal">
-        <div onClick={props.toggleModal} className="overlay"></div>
-        <div className="modal-content">
-          {props.method === 'deposit' &&<input type="Amount:" id="amount-label" name="depositValue" value={depositValue} placeholder="Enter Amount" onChange={handleChangeDeposit} />}
-          {props.method === 'withdraw' &&<input type="Amount:" id="amount-label" name="withdrawValue" value={withdrawValue} placeholder="Enter Amount" onChange={handleChangeWithdraw} />}
-          {props.method === 'transfer' &&<input type="number" id="id-label" name="transferNewId" value={transferNewId} placeholder="Enter ID" onChange={handleTransferId} />}
-          {props.method === 'transfer' &&<input type="Amount:" id="amount-label" name="transferValue" value={transferValue} placeholder="Enter Amount" onChange={handleChangeTransfer} />}
-          <button  onClick={handleSubmit}>Submit</button>
-          <button className="close-modal" onClick={props.toggleModal}>
-            x
-          </button>
->>>>>>> parent of 215a062 (update)
-        </div>
-      </div>
-    )}
-
-      </>
-);
 }
