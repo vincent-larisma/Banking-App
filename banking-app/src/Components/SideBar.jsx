920d3e9117da4b4dbffe7030a1d3a1656c3e9244
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-10 16:16:45 +0800

    Update design

--- banking-app/src/Components/SideBar.jsx
+++ banking-app/src/Components/SideBar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function SideBar() {
   return (
     <>
-      <div className='side-bar-container has-background-primary-light '>
-        <div className='navbar-brand'>
-          <div className='navbar-item is-size-4'>
+      <div className='side-bar-container has-background-link  '>
+        <div className='navbar-brand '>
+          <div className='navbar-item is-size-4 has-text-white'>
             <span class='icon-text'>
               <span class='icon'>
                 <i class='fa-solid fa-earth-europe'></i>
@@ -14,31 +14,31 @@ export default function SideBar() {
             </span>
           </div>
         </div>
-        <aside className='menu'>
-          <div className='container subtitle'>User</div>
-          <p className='menu-label'>Transactions</p>
-          <ul className='menu-list'>
+        <aside className='menu '>
+          <div className='container subtitle text-center has-text-white'>User</div>
+          <p className='menu-label pl-2 has-text-white '>Transactions</p>
+          <ul className='menu-list pl-3 '>
             <li>
-              <a>
-                <i class='fas fa-home'></i> Withdraw
+              <a className='has-text-white '>
+                <i class='fas fa-home '></i> Withdraw
               </a>
             </li>
             <li>
-              <a>
+              <a className='has-text-white'>
                 <i class='fas fa-home'></i> Desposit
               </a>
             </li>
             <li>
-              <a>
+              <a className='has-text-white'>
                 <i class='fas fa-home'></i> Transfer
               </a>
             </li>
           </ul>
 
-          <p className='menu-label'>Logout</p>
-          <ul className='menu-list'>
+          <p className='menu-label pl-2 has-text-white'>Logout</p>
+          <ul className='menu-list pl-3'>
             <li>
-              <a>
+              <a className='has-text-white'>
                 <i class='fas fa-home'></i>
               </a>
             </li>
--- banking-app/src/Components/UserPage.jsx
+++ banking-app/src/Components/UserPage.jsx
@@ -5,19 +5,34 @@ export default function UserPage() {
   return (
     <>
       <SideBar />
-      <div className='sidebar-margin'>
-        {/* <div class='columns'>
-        <div class='column'>Hellosaidiyashduashuidhauihduiahuidshau</div>
-        <div class='column'>Second column</div>
-        <div class='column'>Third column</div>
-        <div class='column'>Fourth column</div>
-      </div> */}
-        <div class='columns is-mobile'>
-          <div class='column'>First column</div>
-          <div class='column'>Second column</div>
-          <div class='column'>Third column</div>
-          <div class='column'>Fourth column</div>
-        </div>
+      <div className='sidebar-margin '>
+        <section className='section notifiication'>
+          <div className='container '>
+            <div class='columns is-multiline '>
+              <div class='column notification is-link is-full '>
+                <h1 className='title'>
+                  <i class='fas fa-home'></i> Home
+                </h1>
+              </div>
+              <div class='column notification is-link '>
+                <h1 className='subtitle'>Name</h1>
+                <h1 className='title'>Name Here</h1>
+                <h1 className='subtitle'>ID</h1>
+                <h1 className='title'>ID here</h1>
+              </div>
+              <div class='column is-0'></div>
+
+              <div class='column notification is-link'>
+                <h1 className='subtitle'>Membership:</h1>
+                <h1 className='title has-text-centered'>Bronze Member</h1>
+              </div>
+            </div>
+            <div class='column notification is-link '>
+              <h1 className='subtitle'>Total Cash Amount:</h1>
+              <h1 className='title has-text-centered'>Add Number Here</h1>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   )
