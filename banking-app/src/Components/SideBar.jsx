9856eb713ea2948ec8ef116c9a2b713cc1abc7b1
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-10 19:45:16 +0800

    Update sidebar design and add history

--- banking-app/src/Components/SideBar.jsx
+++ banking-app/src/Components/SideBar.jsx
@@ -1,49 +1,77 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function SideBar() {
+  const navigate = useNavigate()
+  const handleClickLogout = () => {
+    navigate('/Login')
+  }
   return (
     <>
-      <div className='side-bar-container has-background-link  '>
-        <div className='navbar-brand '>
-          <div className='navbar-item is-size-4 has-text-white'>
-            <span class='icon-text'>
-              <span class='icon'>
-                <i class='fa-solid fa-earth-europe'></i>
+      <div className='side-bar-container has-background-black  '>
+        <div className='flex-item'>
+          <div className='navbar-brand '>
+            <div className='navbar-item is-size-4 has-text-white'>
+              <span class='icon-text'>
+                <span class='icon'>
+                  <i class='fa-solid fa-earth-europe'></i>
+                </span>
+                <span>BANK</span>
               </span>
-              <span>BANK</span>
-            </span>
+            </div>
           </div>
+          <aside className='menu '>
+            <ul className='menu-list pl-3 pt-3  '>
+              <li>
+                <a className='has-text-white is-active'>
+                  <i class='fas fa-home '></i> Home
+                </a>
+              </li>
+            </ul>
+            <p className='menu-label pl-2 has-text-white pt-4'>Transactions</p>
+            <ul className='menu-list pl-3 '>
+              <li>
+                <a className='has-text-white '>
+                  <h1>
+                    <h1></h1> <i class='fas fa-home '></i> Withdraw
+                  </h1>
+                </a>
+              </li>
+              <li>
+                <a className='has-text-white'>
+                  <h1>
+                    <i class='fas fa-home'></i> Desposit
+                  </h1>
+                </a>
+              </li>
+              <li>
+                <a className='has-text-white'>
+                  <h1>
+                    <i class='fas fa-home'></i> Transfer
+                  </h1>
+                </a>
+              </li>
+              <li>
+                <a className='has-text-white'>
+                  <h1>
+                    <i class='fas fa-home'></i> History
+                  </h1>
+                </a>
+              </li>
+            </ul>
+          </aside>
         </div>
-        <aside className='menu '>
-          <div className='container subtitle text-center has-text-white'>User</div>
-          <p className='menu-label pl-2 has-text-white '>Transactions</p>
-          <ul className='menu-list pl-3 '>
-            <li>
-              <a className='has-text-white '>
-                <i class='fas fa-home '></i> Withdraw
-              </a>
-            </li>
-            <li>
-              <a className='has-text-white'>
-                <i class='fas fa-home'></i> Desposit
-              </a>
-            </li>
-            <li>
-              <a className='has-text-white'>
-                <i class='fas fa-home'></i> Transfer
-              </a>
-            </li>
-          </ul>
-
-          <p className='menu-label pl-2 has-text-white'>Logout</p>
+        <div className='flex-item mb-6'>
           <ul className='menu-list pl-3'>
             <li>
-              <a className='has-text-white'>
-                <i class='fas fa-home'></i>
+              <a className='has-text-white' onClick={handleClickLogout}>
+                <h1>
+                  <i class='fa-solid fa-right-from-bracket'></i> Logout
+                </h1>
               </a>
             </li>
           </ul>
-        </aside>
+        </div>
       </div>
     </>
   )
--- banking-app/src/Components/UserPage.jsx
+++ banking-app/src/Components/UserPage.jsx
@@ -15,20 +15,28 @@ export default function UserPage() {
                 </h1>
               </div>
               <div class='column notification is-link '>
-                <h1 className='subtitle'>Name</h1>
-                <h1 className='title'>Name Here</h1>
-                <h1 className='subtitle'>ID</h1>
-                <h1 className='title'>ID here</h1>
+                <h1 className='subtitle'>
+                  <i class='fa-solid fa-address-card'></i> Name
+                </h1>
+                <h1 className='title pl-3'>Name Here</h1>
+                <h1 className='subtitle'>
+                  <i class='fa-solid fa-id-card-clip'></i> ID
+                </h1>
+                <h1 className='title pl-3'>ID here</h1>
               </div>
               <div class='column is-0'></div>
 
               <div class='column notification is-link'>
-                <h1 className='subtitle'>Membership:</h1>
+                <h1 className='subtitle'>
+                  <i class='fa-solid fa-credit-card'></i> Membership:
+                </h1>
                 <h1 className='title has-text-centered'>Bronze Member</h1>
               </div>
             </div>
             <div class='column notification is-link '>
-              <h1 className='subtitle'>Total Cash Amount:</h1>
+              <h1 className='subtitle'>
+                <i class='fa-solid fa-wallet'></i> Total Cash Amount:
+              </h1>
               <h1 className='title has-text-centered'>Add Number Here</h1>
             </div>
           </div>
