013fd6ad1cfb0ef04ccb2f4009e7e0af81ce0e6e
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-10 12:17:50 +0800

    Add sidebar draft and add scss file to userpage

--- banking-app/src/App.js
+++ banking-app/src/App.js
@@ -4,12 +4,14 @@ import { Routes, Route } from 'react-router-dom'
 //Component
 import LoginPage from './Components/LoginPage'
 import Dashboard from './Components/Dashboard'
+import UserPage from './Components/UserPage'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/Dashboard' element={<Dashboard />}></Route>
+        <Route path='/User' element={<UserPage />}></Route>
         <Route path='/Login' element={<LoginPage />}></Route>
         <Route path='/' element={<LoginPage />}></Route>
       </Routes>
--- banking-app/src/Components/UserPage.jsx
+++ banking-app/src/Components/UserPage.jsx
@@ -3,7 +3,47 @@ import React from 'react'
 export default function UserPage() {
   return (
     <>
-    
+      <div className='sideBar'>
+        <aside className='menu'>
+          <div className='navbar-brand'>
+            <div className='navbar-item is-size-4'>
+              <span class='icon-text'>
+                <span class='icon'>
+                  <i class='fa-solid fa-earth-europe'></i>
+                </span>
+                <span>BANK</span>
+              </span>
+            </div>
+          </div>
+          <div className='container subtitle'>User</div>
+          <p className='menu-label'>Transactions</p>
+          <ul className='menu-list'>
+            <li>
+              <a>
+                <i class='fas fa-home'></i> Withdraw
+              </a>
+            </li>
+            <li>
+              <a>
+                <i class='fas fa-home'></i> Desposit
+              </a>
+            </li>
+            <li>
+              <a>
+                <i class='fas fa-home'></i> Transfer
+              </a>
+            </li>
+          </ul>
+          <p className='menu-label'>Logout</p>
+          <ul className='menu-list'>
+            <li>
+              <a>
+                <i class='fas fa-home'></i>
+              </a>
+            </li>
+          </ul>
+        </aside>
+      </div>
     </>
   )
 }
