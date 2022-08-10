c0f86554376c089ed623105b36d36a3997b7b958
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-10 13:34:01 +0800

    Update draft sidebar - added container

---
+++ banking-app/src/Components/SideBar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default function SideBar() {
+  return (
+    <>
+      <div className='side-bar-container has-background-primary-light '>
+        <div className='navbar-brand'>
+          <div className='navbar-item is-size-4'>
+            <span class='icon-text'>
+              <span class='icon'>
+                <i class='fa-solid fa-earth-europe'></i>
+              </span>
+              <span>BANK</span>
+            </span>
+          </div>
+        </div>
+        <aside className='menu'>
+          <div className='container subtitle'>User</div>
+          <p className='menu-label'>Transactions</p>
+          <ul className='menu-list'>
+            <li>
+              <a>
+                <i class='fas fa-home'></i> Withdraw
+              </a>
+            </li>
+            <li>
+              <a>
+                <i class='fas fa-home'></i> Desposit
+              </a>
+            </li>
+            <li>
+              <a>
+                <i class='fas fa-home'></i> Transfer
+              </a>
+            </li>
+          </ul>
+
+          <p className='menu-label'>Logout</p>
+          <ul className='menu-list'>
+            <li>
+              <a>
+                <i class='fas fa-home'></i>
+              </a>
+            </li>
+          </ul>
+        </aside>
+      </div>
+    </>
+  )
+}
--- banking-app/src/Components/UserPage.jsx
+++ banking-app/src/Components/UserPage.jsx
@@ -1,48 +1,23 @@
 import React from 'react'
+import SideBar from './SideBar'
 
 export default function UserPage() {
   return (
     <>
-      <div className='sideBar'>
-        <aside className='menu'>
-          <div className='navbar-brand'>
-            <div className='navbar-item is-size-4'>
-              <span class='icon-text'>
-                <span class='icon'>
-                  <i class='fa-solid fa-earth-europe'></i>
-                </span>
-                <span>BANK</span>
-              </span>
-            </div>
-          </div>
-          <div className='container subtitle'>User</div>
-          <p className='menu-label'>Transactions</p>
-          <ul className='menu-list'>
-            <li>
-              <a>
-                <i class='fas fa-home'></i> Withdraw
-              </a>
-            </li>
-            <li>
-              <a>
-                <i class='fas fa-home'></i> Desposit
-              </a>
-            </li>
-            <li>
-              <a>
-                <i class='fas fa-home'></i> Transfer
-              </a>
-            </li>
-          </ul>
-          <p className='menu-label'>Logout</p>
-          <ul className='menu-list'>
-            <li>
-              <a>
-                <i class='fas fa-home'></i>
-              </a>
-            </li>
-          </ul>
-        </aside>
+      <SideBar />
+      <div className='sidebar-margin'>
+        {/* <div class='columns'>
+        <div class='column'>Hellosaidiyashduashuidhauihduiahuidshau</div>
+        <div class='column'>Second column</div>
+        <div class='column'>Third column</div>
+        <div class='column'>Fourth column</div>
+      </div> */}
+        <div class='columns is-mobile'>
+          <div class='column'>First column</div>
+          <div class='column'>Second column</div>
+          <div class='column'>Third column</div>
+          <div class='column'>Fourth column</div>
+        </div>
       </div>
     </>
   )
