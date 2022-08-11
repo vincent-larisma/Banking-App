23a272546332607e2ec257b8bfa65e8d977599df
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-11 14:40:04 +0800

    Draft Design Pages

--- banking-app/src/Components/TransferPage.jsx
+++ banking-app/src/Components/TransferPage.jsx
@@ -12,6 +12,20 @@ export default function TransferPage() {
                   <i class='fa-solid fa-money-bill-transfer'></i> Transfer
                 </h1>
               </div>
+              <div className='column notification is-link '>
+                <h1 className='subtitle'>Amount Withdrawn: </h1>
+                <div className='container'>
+                  <input class='input is-link  is-large mb-2' type='number' placeholder='$ Amount' />
+                  <button class='button is-success has-text-centered'>Confirmed</button>
+                </div>
+              </div>
+              <div className='column is-0 '></div>
+              <div className='column notification is-link'>
+                <h1 className='subtitle'>Total Amount in Bank: </h1>
+                <div className='container notification is-link is-light'>
+                  <h1 className='title has-text-centered'>Add Number Here</h1>
+                </div>
+              </div>
             </div>
           </div>
         </section>
--- banking-app/src/Components/WithdrawPage.jsx
+++ banking-app/src/Components/WithdrawPage.jsx
@@ -5,12 +5,24 @@ export default function WithdrawPage() {
     <>
       <div className='sidebar-margin '>
         <section className='section notifiication'>
-          <div className='container '>
-            <div class='columns is-multiline '>
-              <div class='column notification is-link is-full '>
-                <h1 className='title'>
-                  <i class='fa-solid fa-money-bills'></i> Withdraw
-                </h1>
+          <div class='columns is-multiline '>
+            <div class='column notification is-link is-full '>
+              <h1 className='title'>
+                <i class='fa-solid fa-money-bills'></i> Withdraw
+              </h1>
+            </div>
+            <div className='column notification is-link '>
+              <h1 className='subtitle'>Amount Withdrawn: </h1>
+              <div className='container'>
+                <input class='input is-link  is-large mb-2' type='number' placeholder='$ Amount' />
+                <button class='button is-success has-text-centered'>Confirmed</button>
+              </div>
+            </div>
+            <div className='column is-0 '></div>
+            <div className='column notification is-link'>
+              <h1 className='subtitle'>Total Amount in Bank: </h1>
+              <div className='container notification is-link is-light'>
+                <h1 className='title has-text-centered'>Add Number Here</h1>
               </div>
             </div>
           </div>
