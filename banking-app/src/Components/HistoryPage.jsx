8e796ade13e34bb06c0294ffd37b951d2cf0c0be
Vince <103049779+vincent-larisma@users.noreply.github.com>
2022-08-11 16:49:44 +0800

    UI page draft done

--- banking-app/src/Components/HistoryPage.jsx
+++ banking-app/src/Components/HistoryPage.jsx
@@ -12,6 +12,23 @@ export default function HistoryPage() {
                   <i class='fa-solid fa-receipt'></i> History
                 </h1>
               </div>
+              <div className='column notification is-link'>
+                <h1 className='title'>Transaction History</h1>
+                <table className='table is-fullwidth is-striped'>
+                  <thead>
+                    <th>Date</th>
+                    <th>Type</th>
+                    <th>Amount</th>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Hello</td>
+                      <th>Hello</th>
+                      <th>Hello</th>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </section>
--- banking-app/src/Components/TransferPage.jsx
+++ banking-app/src/Components/TransferPage.jsx
@@ -12,18 +12,21 @@ export default function TransferPage() {
                   <i class='fa-solid fa-money-bill-transfer'></i> Transfer
                 </h1>
               </div>
-              <div className='column notification is-link '>
-                <h1 className='subtitle'>Amount Withdrawn: </h1>
-                <div className='container'>
-                  <input class='input is-link  is-large mb-2' type='number' placeholder='$ Amount' />
-                  <button class='button is-success has-text-centered'>Confirmed</button>
-                </div>
-              </div>
-              <div className='column is-0 '></div>
-              <div className='column notification is-link'>
+              <div className='column notification is-link is-full'>
                 <h1 className='subtitle'>Total Amount in Bank: </h1>
                 <div className='container notification is-link is-light'>
                   <h1 className='title has-text-centered'>Add Number Here</h1>
+                </div>
+              </div>
+              <div className='column notification is-link is-half is-offset-one-quarter'>
+                <div className='column'>
+                  <h1 className='subtitle'>Reciever's ID:</h1>
+                  <input class='input is-link  is-large' type='number' placeholder='Input ID' />
+                </div>
+                <div className='column'>
+                  <h1 className='subtitle'>Amount Transferable: </h1>
+                  <input class='input is-link  is-large mb-2' type='number' placeholder='$ Amount' />
+                  <button class='button is-success has-text-centered'>Confirmed</button>
                 </div>
               </div>
             </div>
